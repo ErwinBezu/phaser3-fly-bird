4933f985e4f0bb5aab857ea1404fce48185acbbb
Erwin Bezu <erwin.bezu@gmail.com>
2024-04-02 20:57:00 +0200

    bird's gravity and velocity

--- src/index.js
+++ src/index.js
@@ -7,21 +7,44 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 200 },
+      debug: true,
+      gravity: {
+        y: 400,
+      },
     },
   },
   scene: {
-    preload: preload,
-    create: create,
+    preload,
+    create,
+    update,
   },
 };
 
-new Phaser.Game(config);
-
 function preload() {
   this.load.image("sky", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
 }
 
+const VELOCITY = 200;
+let bird = null;
+let flapVelocity = 250;
+let totalDelta = null;
+
 function create() {
-  this.add.image(400, 300, "sky");
+  // this.add.image(config.width / 2, config.height / 2, "sky");
+  this.add.image(0, 0, "sky").setOrigin(0, 0);
+  bird = this.physics.add
+    .sprite(config.width / 10, config.height / 2, "bird")
+    .setOrigin(0);
+
+  this.input.on("pointerdown", flap);
+  this.input.keyboard.on("keydown_SPACE", flap);
 }
+
+function update(time, delta) {}
+
+function flap() {
+  bird.body.velocity.y = -flapVelocity;
+}
+
+new Phaser.Game(config);
