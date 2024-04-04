7502d8488d716de268984ec82d02d1ec3e604bfe
Erwin Bezu <erwin.bezu@gmail.com>
2024-04-04 09:50:31 +0200

    restart bird out of bounds

--- src/index.js
+++ src/index.js
@@ -27,21 +27,31 @@ function preload() {
 
 const VELOCITY = 200;
 let bird = null;
-let flapVelocity = 250;
-let totalDelta = null;
+const flapVelocity = 250;
+const initalBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
 
 function create() {
   // this.add.image(config.width / 2, config.height / 2, "sky");
   this.add.image(0, 0, "sky").setOrigin(0, 0);
   bird = this.physics.add
-    .sprite(config.width / 10, config.height / 2, "bird")
+    .sprite(initalBirdPosition.x, initalBirdPosition.y, "bird")
     .setOrigin(0);
 
   this.input.on("pointerdown", flap);
   this.input.keyboard.on("keydown_SPACE", flap);
 }
 
-function update(time, delta) {}
+function update(time, delta) {
+  if (bird.y > config.height || bird.y < -bird.height) {
+    restartBirdPosition();
+  }
+}
+
+function restartBirdPosition() {
+  bird.x = initalBirdPosition.x;
+  bird.y = initalBirdPosition.y;
+  bird.body.velocity.y = 0;
+}
 
 function flap() {
   bird.body.velocity.y = -flapVelocity;
